e1c0feb837b2f702ad0cb65e26bdc5afbfab3e8d
bigtyno931128 <bigtyno4@gmail.com>
2023-07-31 12:18:16 +0900

    파이프 라인 구축과 기본 셋팅

---
+++ src/test/java/com/hehehe/HeheheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehehe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeheheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
